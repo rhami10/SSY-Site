4150e28f15d4d18028603493f8efdc3342034fc7
rhami1 <hami159@gmail.com>
2022-04-25 19:41:20 +0200

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import Stage from './Stage'
 const APP = window.APP || {}
 
 const camp_det = [
-    {name: "savio", title: "SSY Savio", title_2: "Sports Camp", images: ["https://static.wixstatic.com/media/f07703_f8ab5d641e0448fe903d717e1a3e7f6f~mv2.jpg", "https://static.wixstatic.com/media/f07703_2abced15466f4e0382cd02eb98d0b633~mv2.jpg"]},
+    {name: "savio", title: "SSY Savio", title_2: "Sports Camp", images: ["/dist/img/tiles/savio/base.jpg", "/dist/img/tiles/savio/hover.jpg"]},
     {name: "bosco", title: "SSY Bosco", title_2: "Sports Camp", images: ["", ""]},
     {name: "creative", title: "SSY Creative", title_2: "Arts Camp", images: ["", ""]},
     {name: "adventure", title: "SSY Adventure", title_2: "Camp", images: ["", ""]},
@@ -24,6 +24,8 @@ const initApp = () => {
     document.getElementById("tileImage").src = camp_det[camp_det_index].images[0];
     document.getElementById("tileImage").setAttribute('data-hover', camp_det[camp_det_index].images[1]);
     document.getElementById("tileImage").setAttribute('alt', camp_det[camp_det_index].title.concat(camp_det[camp_det_index].title_2));
+    document.getElementById
+
 
     window.APP = APP
     APP.Stage = new Stage(shader_type)
